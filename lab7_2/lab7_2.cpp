babfece49a1ac8bc0b7d2be075b8a3f6a14a263c
Alexander Veselov <Alexander_Veselov@yahoo.com>
2018-10-11 00:07:25 +0300

    Add determinant calculation

--- lab7_2/lab7_2.cpp
+++ lab7_2/lab7_2.cpp
@@ -2,28 +2,33 @@
 #include <omp.h>
 #include <iostream>
 #include <vector>
-#include <array>
 
-template <typename T, std::size_t size>
+template <typename T>
 class Matrix
 {
 public:
-    Matrix()
+    Matrix(std::size_t size)
+        : size_(size)
     {
+        data_.resize(size_ * size_);
         std::fill(data_.begin(), data_.end(), 0);
     }
 
-    Matrix(T const* data)
+    Matrix(T const* data, std::size_t size)
+        : size_(size)
     {
-        std::copy(data, data + size * size, data_);
+        data_.resize(size_ * size_);
+        std::copy(data, data + size_ * size_, data_);
     }
 
     Matrix(Matrix const& other)
         : data_(other.data_)
+        , size_(other.size_)
     {}
 
     Matrix(Matrix && other)
         : data_(std::move(other.data_))
+        , size_(std::move(other.size_))
     {}
 
     T& operator[](std::size_t index)
@@ -38,24 +43,24 @@ public:
 
     std::size_t GetSize() const
     {
-        return size;
+        return size_;
     }
 
     T& Element(std::size_t row, std::size_t column)
     {
-        return data_[row * size + column];
+        return data_[row * size_ + column];
     }
 
     T const& Element(std::size_t row, std::size_t column) const
     {
-        return data_[row * size + column];
+        return data_[row * size_ + column];
     }
 
-    Matrix<T, size - 1> GetCofactor(std::size_t row, std::size_t column) const
+    Matrix<T> GetCofactor(std::size_t row, std::size_t column) const
     {
-        Matrix<T, size - 1> cofactor;
+        Matrix<T> cofactor(size_ - 1);
         std::size_t dst_row = 0;
-        for (std::size_t src_row = 0; src_row < size; ++src_row)
+        for (std::size_t src_row = 0; src_row < size_; ++src_row)
         {
             if (src_row == row)
             {
@@ -63,7 +68,7 @@ public:
             }
 
             std::size_t dst_column = 0;
-            for (std::size_t src_column = 0; src_column < size; ++src_column)
+            for (std::size_t src_column = 0; src_column < size_; ++src_column)
             {
                 if (src_column == column)
                 {
@@ -81,38 +86,62 @@ public:
         return cofactor;
     }
 
+    T Determinant() const
+    {
+        if (size_ == 1)
+        {
+            return data_[0];
+        }
+
+        T result = {0};
+
+        for (std::size_t i = 0; i < GetSize(); ++i)
+        {
+            T element = Element(0, i);
+            T det = GetCofactor(0, i).Determinant();
+            T value = element * det;
+            result += (i % 2) ? -value : value;
+        }
+
+        return result;
+    }
+
 private:
-    std::array<T, size * size> data_;
+    std::vector<T> data_;
+    std::size_t size_;
 
 };
 
-template <typename T, std::size_t size>
-std::ostream& operator<<(std::ostream & os, Matrix<T, size> const& matrix)
+template <typename T>
+std::ostream& operator<<(std::ostream & os, Matrix<T> const& matrix)
 {
-    for (std::size_t i = 0; i < size; ++i)
+    os << "{";
+    for (std::size_t i = 0; i < matrix.GetSize(); ++i)
     {
-        for (std::size_t j = 0; j < size; ++j)
+        os << "{";
+        for (std::size_t j = 0; j < matrix.GetSize(); ++j)
         {
-            os << matrix[i * size + j] << " ";
+            os << matrix[i * matrix.GetSize() + j] << ", ";
         }
-        os << std::endl;
+        os << "},";
     }
+    os << "}";
     return os;
 }
 
 int main(int argc, char** argv)
 {
-    Matrix<std::uint32_t, 3> matrix;
-    for (int i = 0; i < 3; ++i)
+    Matrix<std::int64_t> matrix(4);
+    for (int i = 0; i < 4; ++i)
     {
-        for (int j = 0; j < 3; ++j)
+        for (int j = 0; j < 4; ++j)
         {
             matrix.Element(i, j) = rand();
         }
     }
 
     std::cout << matrix << std::endl;
-    std::cout << matrix.GetCofactor(1, 0) << std::endl;
+    std::cout << matrix.Determinant() << std::endl;
 
     return 0;
 }
