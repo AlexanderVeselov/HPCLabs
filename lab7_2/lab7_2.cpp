3604dd5172a3f3ebfda3b116b778ef3e1d104982
Alexander Veselov <Alexander_Veselov@yahoo.com>
2018-10-12 23:59:36 +0300

    Add determinant calculation using condensation method

--- lab7_2/lab7_2.cpp
+++ lab7_2/lab7_2.cpp
@@ -1,7 +1,10 @@
+#include "../utils/time_profiler.hpp"
 #include <cmath>
 #include <omp.h>
 #include <iostream>
 #include <vector>
+#include <stack>
+#include <algorithm>
 
 template <typename T>
 class Matrix
@@ -11,7 +14,7 @@ public:
         : size_(size)
     {
         data_.resize(size_ * size_);
-        std::fill(data_.begin(), data_.end(), 0);
+        std::fill(data_.begin(), data_.end(), (T)0);
     }
 
     Matrix(T const* data, std::size_t size)
@@ -25,6 +28,13 @@ public:
         : data_(other.data_)
         , size_(other.size_)
     {}
+
+    Matrix& operator=(Matrix const& other)
+    {
+        data_ = other.data_;
+        size_ = other.size_;
+        return *this;
+    }
 
     Matrix(Matrix && other)
         : data_(std::move(other.data_))
@@ -86,6 +96,21 @@ public:
         return cofactor;
     }
 
+    Matrix<T> GetInner() const
+    {
+        Matrix<T> inner(size_ - 2);
+        for (std::size_t i = 1; i < size_ - 1; ++i)
+        {
+            for (std::size_t j = 1; j < size_ - 1; ++j)
+            {
+                inner.Element(i - 1, j - 1) = Element(i, j);
+            }
+        }
+
+        return inner;
+
+    }
+
     T Determinant() const
     {
         if (size_ == 1)
@@ -94,12 +119,10 @@ public:
         }
 
         T result = {0};
-
-        for (std::size_t i = 0; i < GetSize(); ++i)
+#pragma omp parallel for reduction (+:result)
+        for (int i = 0; i < GetSize(); ++i)
         {
-            T element = Element(0, i);
-            T det = GetCofactor(0, i).Determinant();
-            T value = element * det;
+            T value = Element(0, i) * GetCofactor(0, i).Determinant();
             result += (i % 2) ? -value : value;
         }
 
@@ -115,33 +138,74 @@ private:
 template <typename T>
 std::ostream& operator<<(std::ostream & os, Matrix<T> const& matrix)
 {
-    os << "{";
+    //os << "{";
     for (std::size_t i = 0; i < matrix.GetSize(); ++i)
     {
-        os << "{";
+        //os << "{";
         for (std::size_t j = 0; j < matrix.GetSize(); ++j)
         {
-            os << matrix[i * matrix.GetSize() + j] << ", ";
+            os << matrix[i * matrix.GetSize() + j] << " ";//", ";
         }
-        os << "},";
+        os << std::endl;
+        //os << "},";
     }
-    os << "}";
+   // os << "}";
     return os;
 }
 
 int main(int argc, char** argv)
 {
-    Matrix<std::int64_t> matrix(4);
-    for (int i = 0; i < 4; ++i)
+    Matrix<float> matrix(5);
+    for (int i = 0; i < matrix.GetSize(); ++i)
     {
-        for (int j = 0; j < 4; ++j)
+        for (int j = 0; j < matrix.GetSize(); ++j)
         {
-            matrix.Element(i, j) = rand();
+            matrix.Element(i, j) = (float)(std::rand() % 30000) + 1;
         }
     }
 
     std::cout << matrix << std::endl;
-    std::cout << matrix.Determinant() << std::endl;
+    {
+        PROFILE_TIME("determinant calculation");
+        std::cout << matrix.Determinant() << std::endl;
+    }
 
+    {
+        PROFILE_TIME("condensation method");
+        Matrix<float> prevprev = matrix;
+        Matrix<float> prev = matrix;
+        for (int iter = 0; iter < matrix.GetSize() - 1; ++iter)
+        {
+            Matrix<float> current(prev.GetSize() - 1);
+            for (int i = 0; i < prev.GetSize() - 1; ++i)
+            {
+                for (int j = 0; j < prev.GetSize() - 1; ++j)
+                {
+                    current.Element(i, j) = prev.Element(i, j) * prev.Element(i + 1, j + 1)
+                        - prev.Element(i, j + 1) * prev.Element(i + 1, j);
+                }
+            }
+            if (iter > 0)
+            {
+                Matrix<float> inner = prevprev.GetInner();
+                //std::cout << current << std::endl;
+                //std::cout << inner << std::endl;
+                //std::cout << prev << std::endl;
+                //std::cout << prevprev << std::endl;
+                for (int i = 0; i < inner.GetSize(); ++i)
+                {
+                    for (int j = 0; j < inner.GetSize(); ++j)
+                    {
+                        //std::cout << current.Element(i, j) << " by " << inner.Element(i, j) << std::endl;
+                        current.Element(i, j) /= inner.Element(i, j);
+
+                    }
+                }
+                prevprev = prev;
+            }
+            prev = current;
+        }
+        std::cout << prev << std::endl;
+    }
     return 0;
 }
